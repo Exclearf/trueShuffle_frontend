a527d116fea109facc6fe2bd6a25969deda72df3
Taras Byalyk <byalykt@hotmail.com>
2024-02-16 11:54:31 +0100

    Basic components

--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -1,20 +1,49 @@
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+const Container = styled.div``;
+
 //@ts-ignore
 const Index = ({ exitHandler }) => {
   return (
-    <div>
-      <header>
-        <h1>Main Page</h1>
-      </header>
-      <main>
-        <p>Main page</p>
-        <nav>
-          <ul>
-            <button onClick={exitHandler}>Log out</button>
-          </ul>
-        </nav>
-      </main>
+    <Container>
+      <div className="spotify_body">
+        <Sidebar/>
+        <div className="body">
+          <Navbar/>
+          <div className="body_contents">
+            <Body/>
+            <nav>
+              <ul>
+              <button onClick={exitHandler}>Log out</button>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    <div className="spotify_footer">
+      <Footer/>
     </div>
+    </Container>
   );
 };
 
 export default Index;
+
+// return (
+//   <div>
+//     <header>
+//       <h1>Main Page</h1>
+//     </header>
+//     <main>
+//       <p>Main page</p>
+//       <nav>
+//         <ul>
+//           <button onClick={exitHandler}>Log out</button>
+//         </ul>
+//       </nav>
+//     </main>
+//   </div>
+// );
